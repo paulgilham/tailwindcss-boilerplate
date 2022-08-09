dc10de91138c67c26a6a18c9a1f5401efa00aaf0
paulgilham <paulgilham1@gmail.com>
2022-08-09 12:24:59 +1000

    added use of partials

--- web/themes/custom/tailwindcss/tailwind.config.js
+++ web/themes/custom/tailwindcss/tailwind.config.js
@@ -1,14 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './templates/*.{html,js,twig}',
     './templates/**/*.{html,js,twig}',
+    './partials/**/*.{html,js,twig}',
+    './partials/**/*.{html,js,twig}',
     './src/twig/*.{html,js,twig}',
     './src/twig/**/*.{html,js,twig}'
   ],
   
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
   plugins: [],
